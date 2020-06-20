14e386a36c2d4919d24fb3d1f8916aca39d13fa7
Ustymenko <ustymyar@ukr.net>
2020-06-20 23:34:36 +0300

    20062020

--- OOPCPP/19062020/19062020/19062020.cpp
+++ OOPCPP/19062020/19062020/19062020.cpp
@@ -2,30 +2,17 @@
 #include <time.h>
 #include <algorithm>
 using namespace std;
+typedef bool (*CMP)(int a, int b);
 class Array {
+private:
 	int N;
 	int* array;
-private:
-	void QuickSort(int left, int right) {
-		int i = left, j = right - 1;
-		int m = array[i + (j - i) / 2];
-		while (i <= j) {
-			while (array[i] < m)i++;
-			while (array[j] > m)j--;
-			if (i <= j) {
-				swap(array[i], array[j]);
-				i++;
-				j--;
-			}
-		}
-		if (left < j)QuickSort(left, j + 1);
-		if (i < right) QuickSort(i, right);
-	}
+	void QuickSort(int left, int right);
 public:
-	void Quicksort() { QuickSort(0, N); }
 	Array();
 	explicit Array(int);
 	Array(const Array& Ar);
+	Array(Array&& a) noexcept;
 	~Array();
 	int Size() const;
 	const int* Data() const;
@@ -34,7 +21,9 @@ public:
 	void Print();
 	void SetRnd(int min, int max);
 	void Input();
+	void Quicksort();
 	Array& operator=(const Array& Ar);//this = Ar
+	Array& operator=(Array&& a)  noexcept;
 	Array operator+(const Array& RAr) const;//this + RAr
 	Array operator+(int el) const; //this + el
 	//Array operator+(int el); //el   + this error
@@ -45,88 +34,16 @@ public:
 	Array& operator--();
 	Array operator--(int);
 	Array operator+();
-	bool operator==(const Array& P)
-	{
-		if (N != P.N) {
-			return false;
-		}
-		/*else {
-		 for (size_t i = 0; i < N; i++)
-		  if (array[i] != P.array[i])return false;
-		}*/
-		/*return true;*/
-		return!memcmp(array, P.array, N * sizeof(*array));
-	}
-	bool operator !=(const Array& P)
-	{
-		return !(*this == P);
-	}
-	bool operator <(const Array& P)
-	{
-		if (N < P.N) {
-			return true;
-		}
-		if (N == P.N)
-			memcmp(array, P.array, N * sizeof(*array)) < 0 ? true : false;
-		return false;
-	}
-	bool operator >(const Array& P)
-	{
-		if (N > P.N) {
-			return true;
-		}
-		if (N == P.N)
-			memcmp(array, P.array, N * sizeof(*array)) > 0 ? true : false;
-		return false;
-	}
-	int operator [](int i) const {
-		if (i >= 0 && i < N)
-			return array[i];
-		exit(1);
-	}
-	int& operator [](int i) {
-		if (i >= 0 && i < N)
-			return array[i];
-		exit(1);
-	}
+	bool operator==(const Array& P);
+	bool operator !=(const Array& P);
+	bool operator <(const Array& P);
+	bool operator >(const Array& P);
+	int operator [](int i) const;
+	int& operator [](int i);
 	friend ostream& operator << (ostream& out, const Array& a);
 	friend istream& operator >> (istream& in, Array& a);
-	Array(Array&& a) noexcept {
-		N = a.N;
-		array = a.array;
-		a.array = nullptr;
-		cout << "MoveArr\n";
-	}
-	 Array& operator=(Array&& a)  noexcept {
-		Clear();
-		N = a.N;
-		array = a.array;
-		a.array = nullptr;
-		cout << "MoveArr=\n";
-		return *this;
-	}
-	void Sort(bool revers = false) {
-		if (revers) {
-			for (int startIndex = 0; startIndex < N - 1; ++startIndex)
-			{
-				int smallestIndex = startIndex;
-				for (int currentIndex = startIndex + 1; currentIndex < N; ++currentIndex)
-					if (array[currentIndex] < array[smallestIndex])
-						smallestIndex = currentIndex;
-				std::swap(array[startIndex], array[smallestIndex]);
-			}
-		}
-		else {
-			for (int startIndex = 0; startIndex < N - 1; ++startIndex)
-			{
-				int smallestIndex = startIndex;
-				for (int currentIndex = startIndex + 1; currentIndex < N; ++currentIndex)
-					if (array[currentIndex] > array[smallestIndex])
-						smallestIndex = currentIndex;
-				std::swap(array[startIndex], array[smallestIndex]);
-			}
-		}
-	}
+	void Sort(bool revers = false);
+
 };
 Array::Array()
 {
@@ -154,6 +71,95 @@ Array::Array(const Array& Ar) :Array(Ar.N)
 	//copy(Ar.array, Ar.array + N, array); 
 	memcpy(array, Ar.array, N * sizeof(*array));
 }
+Array::Array(Array&& a) noexcept {
+	N = a.N;
+	array = a.array;
+	a.array = nullptr;
+	//cout << "MoveArr\n";
+}
+Array& Array::operator=(Array&& a)  noexcept {
+	Clear();
+	N = a.N;
+	array = a.array;
+	a.array = nullptr;
+	//cout << "MoveArr=\n";
+	return *this;
+}
+Array::~Array()
+{
+	//cout << "destroy\n" << endl;
+	Clear();
+}
+int Array::Size() const
+{
+	return N;
+}
+int* Array::Data()
+{
+	return array;
+}
+const int* Array::Data() const
+{
+	return array;
+}
+void Array::Clear()
+{
+	N = 0;
+	delete[] array;
+	array = nullptr;
+}
+void Array::Print()
+{
+	for (int i = 0; i < N; i++)
+		cout << array[i] << "\t";
+	cout <<"\n----------------------------------------\n";
+}
+void Array::SetRnd(int min, int max)
+{
+	if (min > max)swap(min, max);
+	for (int i = 0; i < N; i++)
+		array[i] = min + rand() % (max - min + 1);
+}
+void Array::Input()
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << "array[" << i << "] = ";
+		cin >> array[i];
+	}
+}
+void Array::QuickSort(int left, int right) {
+	int i = left, j = right - 1;
+	int m = array[i + (j - i) / 2];
+	while (i <= j) {
+		while (array[i] < m)i++;
+		while (array[j] > m)j--;
+		if (i <= j) {
+			swap(array[i], array[j]);
+			i++;
+			j--;
+		}
+	}
+	if (left < j)QuickSort(left, j + 1);
+	if (i < right) QuickSort(i, right);
+}
+void Array::Quicksort() { QuickSort(0, N); }
+void Array::Sort(bool revers) {
+	CMP cmp = NULL;
+	if (revers)
+		cmp = [](int a, int b) { return a > b; };
+	else
+		cmp = [](int a, int b) { return a < b; };
+	for (int i = 0; i < N - 1; ++i)
+	{
+		int pos = i;
+		for (int j = i + 1; j < N; ++j)
+			if (cmp(array[j], array[pos]))
+				pos = j;
+		std::swap(array[i], array[pos]);
+	}
+}
+
 Array& Array::operator=(const Array& Ar)
 {
 	//cout << "operator=" << endl;
@@ -183,7 +189,6 @@ Array Array::operator+(int el) const
 	nar.array[N] = el;
 	return nar;
 }
-
 Array Array::operator-()
 {
 	Array temp(*this);
@@ -191,8 +196,7 @@ Array Array::operator-()
 		temp.array[i] *= -1;
 	return temp;
 }
-
-Array& Array::  operator++()
+Array& Array::operator++()
 {
 	for (size_t i = 0; i < N; ++i)
 	{
@@ -206,7 +210,7 @@ Array Array::operator++(int)
 	++* this;
 	return temp;
 }
-Array& Array::  operator--()
+Array& Array::operator--()
 {
 	for (size_t i = 0; i < N; ++i)
 		--array[i];
@@ -222,50 +226,50 @@ Array Array::operator+()
 {
 	return *this;
 }
-Array::~Array()
+bool Array::operator==(const Array& P)
 {
-	cout << "destroy\n" << endl;
-	Clear();
-}
-int Array::Size() const
-{
-	return N;
-}
-int* Array::Data()
-{
-	return array;
-}
-const int* Array::Data() const
-{
-	return array;
-}
-void Array::Clear()
-{
-	N = 0;
-	delete[] array;
-	array = nullptr;
-}
-void Array::Print()
-{
-	for (int i = 0; i < N; i++)
-		cout << array[i] << "\t";
-	cout << endl;
-}
-void Array::SetRnd(int min, int max)
-{
-	if (min > max)swap(min, max);
-	for (int i = 0; i < N; i++)
-		array[i] = min + rand() % (max - min + 1);
-}
-void Array::Input()
-{
-	for (int i = 0; i < N; i++)
-	{
-		cout << "array[" << i << "] = ";
-		cin >> array[i];
+	if (N != P.N) {
+		return false;
 	}
+	/*else {
+	 for (size_t i = 0; i < N; i++)
+	  if (array[i] != P.array[i])return false;
+	}*/
+	/*return true;*/
+	return !memcmp(array, P.array, N * sizeof(*array));
 }
-
+bool  Array::operator !=(const Array& P)
+{
+	return !(*this == P);
+}
+bool  Array::operator <(const Array& P)
+{
+	if (N < P.N) {
+		return true;
+	}
+	if (N == P.N)
+		memcmp(array, P.array, N * sizeof(*array)) < 0 ? true : false;
+	return false;
+}
+bool  Array::operator >(const Array& P)
+{
+	if (N > P.N) {
+		return true;
+	}
+	if (N == P.N)
+		memcmp(array, P.array, N * sizeof(*array)) > 0 ? true : false;
+	return false;
+}
+int  Array::operator [](int i) const {
+	if (i >= 0 && i < N)
+		return array[i];
+	exit(1);
+}
+int& Array::operator[](int i) {
+	if (i >= 0 && i < N)
+		return array[i];
+	exit(1);
+}
 
 //Array operator+(int el, const Array& Rar) { // int + Array
 // Array nar(Rar.Size() + 1);
@@ -361,11 +365,19 @@ int main()
 	}
 	{
 		Array P(3);
-		P = foo(); 
+		P = foo();
 	}
-	mas2.Sort(false);
-	mas2.Print();
-
-	mas2.Quicksort();
-	mas2.Print();
+	cout << "\n";
+	Array mas4(10);
+	mas4.SetRnd(-100, 100);
+	mas4.Print();
+	mas4.Sort();
+	mas4.Print();
+	mas4.Sort(true);
+	mas4.Print();
+	cout << "\nQuicksort\n";
+	mas4.SetRnd(-100, 100);
+	mas4.Print();
+	mas4.Quicksort();
+	mas4.Print();
 }
